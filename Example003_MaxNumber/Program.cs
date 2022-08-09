19de639358e0e141f4c2f1e982409863dfc4d573
Денис <denis070619870@rambler.ru>
2022-08-09 17:16:48 +0300

    Example003_MaxNumber

---
+++ Example003_MaxNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти максимальное из трех чисел
+
+int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+int c = new Random().Next(1, 10);
+Console.WriteLine(c);
+
+if ( a > b && a > c)
+{
+    Console.WriteLine(a + " " + "Наибольшее число");
+}
+if ( b > a && b > c)
+{
+    Console.WriteLine(b + " " + "Наибольшее число");
+}
+else
+{
+    Console.WriteLine(c + " " + "Наибольшее число");
+}
+
