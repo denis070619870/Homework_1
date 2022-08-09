8c82c3c52fdaa5b416d0d10aa194e023adb0b983
Денис <denis070619870@rambler.ru>
2022-08-09 17:12:37 +0300

    Example002_DayOfTheWeek

---
+++ Example002_DayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+
+if (numberDay == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberDay == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (numberDay == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (numberDay == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (numberDay == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (numberDay == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberDay == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
