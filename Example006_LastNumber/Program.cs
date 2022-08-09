93f3bb26df9bdb7671562440ccce34a3d8b31f9f
Денис <denis070619870@rambler.ru>
2022-08-09 18:17:01 +0300

    Example006_LastNumber

---
+++ Example006_LastNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Ввеите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    int result = n % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное. Ввеите трехзначное число: ");
+}
