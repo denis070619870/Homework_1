c1a4d24ab83c2d65468da0af740ecf75dc64aaa3
Денис <denis070619870@rambler.ru>
2022-08-09 17:19:23 +0300

    Example004_EvenNumber

---
+++ Example004_EvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+if(userNumber % 2 ==0)
+{
+    Console.WriteLine(userNumber +" "+ "Является четным");
+}
+else
+{
+    Console.WriteLine(userNumber +" "+ "Не является четным");
+}
