d3869db04ced702c87d503057f7058802e8a1cb2
Денис <denis070619870@rambler.ru>
2022-08-09 17:07:00 +0300

    Example001_CheckSquare

---
+++ Example001_CheckSquare/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+
+if(A * A == B)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
