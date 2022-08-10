3e99b061c38ed83555fdcfc2e2068ba666949cb5
Денис <denis070619870@rambler.ru>
2022-08-10 10:43:34 +0300

    Example007_SecondNumber

--- Example007_SecondNumber/Program.cs
+++ Example007_SecondNumber/Program.cs
@@ -5,12 +5,10 @@ int n = int.Parse(Console.ReadLine());
 
 if (n >= 100 && n < 1000)
 {
-    int last = n % 10;
-    int first = n / 100;
-    int result = first * 10 + last;
-    Console.WriteLine("Номер без второго числа:" + " " + result);
+    int second = n / 10 % 10;
+    
+    Console.WriteLine("Номер второго числа:" + " " + second);
 }
 else
-{
     Console.WriteLine("Число не трехзначное. Ввеите трехзначное число: ");
-}
+
