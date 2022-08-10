72006c1ac9e29c9338c38525f47b40ca6f87b274
Денис <denis070619870@rambler.ru>
2022-08-10 10:18:14 +0300

    Example008_HighastValue

---
+++ Example008_HighastValue/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 100);
+int Max = 0;
+int MaxNumber()
+{
+    int OneNumber = a / 10;
+    int TwoNumber = a % 10;
+    if (OneNumber > TwoNumber) Max = OneNumber;
+        
+    else Max = TwoNumber;
+
+    return Max;
+}
+MaxNumber();
+System.Console.WriteLine("В числе" + " " + a + " " + "наибольшая цифра" + " " + Max);
