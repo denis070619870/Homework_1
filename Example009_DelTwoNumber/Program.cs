9818e14529c718f543ac3b8711e630840b9b6ffb
Денис <denis070619870@rambler.ru>
2022-08-10 10:23:12 +0300

    Example009_DelTwoNumber

---
+++ Example009_DelTwoNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Ввеите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    int last = n % 10;
+    int first = n / 100;
+    int result = first * 10 + last;
+    Console.WriteLine("Номер без второго числа:" + " " + result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное. Ввеите трехзначное число: ");
+}
